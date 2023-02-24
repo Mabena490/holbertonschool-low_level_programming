1c59eb4348aa7d48181276852d2e2ff414f9e1c6
Mabena490 <mabenanaphty@gmail.com>
2023-02-23 21:25:21 -0800

    Task 7

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -1,7 +1,7 @@
 /*
  * File: 7-print_diagonal.c
  * Auth: Andronica Maswanganye
- 0;136;0c */
+ */
 
 #include "main.h"
 
@@ -11,22 +11,22 @@
  */
 void print_diagonal(int n)
 {
-  int len, space;
+	int len, space;
 
-  if (n > 0)
-    {
-      for (len = 0; len < n; len++)
+	if (n > 0)
 	{
-	  for (space = 0; space < len; space++)
-	    _putchar(' ');
-	  _putchar('\\');
+		for (len = 0; len < n; len++)
+		{
+			for (space = 0; space < len; space++)
+				_putchar(' ');
+			_putchar('\\');
 
-	  if (len == n - 1)
-	    continue;
+			if (len == n - 1)
+				continue;
 
-	  _putchar('\n');
+			_putchar('\n');
+		}
 	}
-    }
 
-  _putchar('\n');
+	_putchar('\n');
 }
