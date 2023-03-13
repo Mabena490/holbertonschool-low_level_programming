d5da1a41509f41a699597ee94a9857fa7e9d0cfc
Mabena490 <mabenanaphty@gmail.com>
2023-03-12 23:31:56 -0700

    Task 0

--- more_malloc_free/0-malloc_checked.c
+++ more_malloc_free/0-malloc_checked.c
@@ -11,10 +11,10 @@
 
 void *malloc_checked(unsigned int b)
 {
-  unsigned int *i;
+	unsigned int *i;
 
-  i = malloc(b);
-  if (i == NULL)
-    exit(98);
-  return (i);
+	i = malloc(b);
+	if (i == NULL)
+		exit(98);
+	return (i);
 }
