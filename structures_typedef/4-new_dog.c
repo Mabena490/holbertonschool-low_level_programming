6b0fbc15f7dd5274a9347610160c35f955e23385
Mabena490 <Mabenanaphty@gmail.com>
2023-03-22 01:50:44 -0700

    Task 4

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -9,29 +9,30 @@
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-int nlen, olen, i;
-dog_t *doggy;
+  int nlen, olen, i;
+  dog_t *doggy;
 
-nlen = olen = 0;
+  nlen = olen = 0;
+  while (name[nlen++])
+    ;
+  while (owner[olen++])
+    ;
+  doggy = malloc(sizeof(dog_t));
+  if (doggy == NULL)
+    return (NULL);
 
-while (name[nlen++])
-;
-while (owner[olen++])
-;
-doggy = malloc(sizeof(dog_t));
-if (doggy == NULL)
-return (NULL);
+  doggy->name = malloc(nlen * sizeof(doggy->name));
+  if (doggy == NULL)
+    return (NULL);
+  for (i = 0; i < nlen; i++)
+    doggy->name[i] = name[i];
 
-doggy->name = malloc(nlen * sizeof(doggy->name));
-if (doggy == NULL)
-return (NULL);
-for (i = 0; i < nlen; i++)
-doggy->name[i] = name[i];
-doggy->age = age;
-doggy->owner = malloc(olen * sizeof(doggy->owner));
-if (doggy == NULL)
-return (NULL); 
-for (i = 0; i < olen; i++)
-doggy->owner[i] = owner[i];
-return (doggy);
+  doggy->age = age;
+
+  doggy->owner = malloc(olen * sizeof(doggy->owner));
+  if (doggy == NULL)
+    return (NULL);
+  for (i = 0; i < olen; i++)
+    doggy->owner[i] = owner[i];
+  return (doggy);
 }
