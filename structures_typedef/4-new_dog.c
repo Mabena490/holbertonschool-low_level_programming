daeb82f073a5679fcfe3b555bed88d5f01cb0ac2
Mabena490 <Mabenanaphty@gmail.com>
2023-03-22 01:59:56 -0700

    Task 4

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -21,7 +21,7 @@ int _strlen(char *str)
 int len = 0;
 
 while (*str++)
-  len++;
+len++;
 
 return (len);
 }
@@ -37,9 +37,8 @@ return (len);
 char *_strcopy(char *dest, char *src)
 {
 int index = 0;
-
 for (index = 0; src[index]; index++)
-  dest[index] = src[index];
+dest[index] = src[index];
 
 dest[index] = '\0';
 
@@ -57,32 +56,26 @@ return (dest);
 dog_t *new_dog(char *name, float age, char *owner)
 {
 dog_t *doggo;
-
 if (name == NULL || age < 0 || owner == NULL)
-  return (NULL);
-
+return (NULL);
 doggo = malloc(sizeof(dog_t));
 if (doggo == NULL)
   return (NULL);
-
 doggo->name = malloc(sizeof(char) * (_strlen(name) + 1));
 if (doggo->name == NULL)
-  {
+{
 free(doggo);
 return (NULL);
 }
-
 doggo->owner = malloc(sizeof(char) * (_strlen(owner) + 1));
 if (doggo->owner == NULL)
-  {
+{
 free(doggo->name);
 free(doggo);
 return (NULL);
 }
-
 doggo->name = _strcopy(doggo->name, name);
 doggo->age = age;
 doggo->owner = _strcopy(doggo->owner, owner);
-
 return (doggo);
 }
